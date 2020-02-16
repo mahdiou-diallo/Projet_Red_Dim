d606a3ce9eb687cf5f3e0de28d14910a2b8ef4a0
mahdiou <mmahdiou.diallo@gmail.com>
2020-02-16 17:21:51 +0100

    correct bugs -- before cleanup

---
+++ r1svd.py
@@ -0,0 +1,151 @@
+import numpy as np
+import pandas as pd
+from numpy.linalg import norm
+
+import matplotlib.pyplot as plt
+
+
+def reorder(M: np.ndarray, idx_r: np.ndarray, idx_c: np.ndarray):
+    return M[np.ix_(idx_r, idx_c)]
+
+
+def reorder_df(df: pd.DataFrame, idx_r: np.ndarray, idx_c: np.ndarray):
+    tmp = df.copy()
+    tmp = tmp.iloc[idx_r, :]
+    tmp = tmp.iloc[:, idx_c]
+    return tmp
+
+
+def _rand_unit(n: int):
+    x = np.random.rand(n)
+    x = np.abs(x)
+    x = x / norm(x)
+    return x
+
+
+def _init_diagonal_matrices(A):
+    Dr = A.sum(axis=1)
+    Dc = A.sum(axis=0)
+
+    Dr[Dr == 0] = 1
+    Dc[Dc == 0] = 1
+
+    Dr1_A = np.diag(1/Dr) @ A
+    Dc1_A = np.diag(1/Dc) @ A.T
+
+    return Dr1_A, Dc1_A
+
+
+class RankOneSvd:
+    """ """
+
+    def __init__(self, threshold=1E-4, max_iter=1000):
+        self.threshold = threshold
+        self.max_iter = max_iter
+
+    def fit(self, A: np.ndarray):
+        self.A_ = A
+        Dr1_A, Dc1_A = _init_diagonal_matrices(A)
+
+        n, m = self.A_.shape
+
+        u = _rand_unit(n)
+        v = _rand_unit(m)
+
+        u_prev = u
+        v_prev = v
+
+        gamma_prev = -np.inf
+
+        it = 0
+
+        while it < self.max_iter:
+            v = Dc1_A @ u_prev
+            v = v/norm(v)
+
+            u = Dr1_A @ v_prev
+            u = u/norm(u)
+
+            gamma = norm(u - u_prev) + norm(v - v_prev)
+
+            if abs(gamma - gamma_prev) <= self.threshold:
+                break
+
+            gamma_prev = gamma
+            u_prev = u
+            v_prev = v
+
+            it += 1
+
+        if it == self.max_iter:
+            print(f'did not converge in {self.max_iter} iterations')
+        # print(f'Converged in {it} iterations.')
+        # print('u = {u}\n v = {v}')
+        self.u_ = u
+        self.v_ = v
+        self.u_order_ = np.argsort(u)
+        self.v_order_ = np.argsort(v)
+        return self
+
+    def fit_transform(self, A: np.ndarray):
+        self.fit(A)
+        return A[np.ix_(self.u_order_, self.v_order_)]
+
+    def get_row_labels(self):
+        du = np.concatenate([[0], np.diff(self.u_[self.u_order_])])
+        m = np.mean(du)
+        return np.cumsum(du > m)
+
+    def get_col_labels(self):
+        dv = np.concatenate([[0], np.diff(self.v_[self.v_order_])])
+        m = np.mean(dv)
+        return np.cumsum(dv > m)
+
+    def get_co_clusters(self):
+        r_idx = self.get_row_labels()
+        c_idx = self.get_col_labels()
+        A_sorted = self.A_[np.ix_(self.u_order_, self.v_order_)]
+
+        nr = r_idx[-1]+1
+        nc = c_idx[-1]+1
+        M = np.zeros((nr, nc))
+        for row in range(nr):
+            for col in range(nc):
+                M[row, col] = A_sorted[
+                    np.ix_(r_idx == row, c_idx == col)].sum()
+        reg_mat = M > M.mean()
+        regions = [*zip(*np.where(reg_mat))]
+        return regions, reg_mat
+
+    def plot_matrix(self, matrix='A', ordered=False):
+        if ordered:
+            if matrix == 'A':
+                plt.spy(self.A_[np.ix_(self.u_order_, self.v_order_)])
+                return
+            if matrix == 'Sr':
+                Sr = self.A_ @ self.A_.T
+                plt.spy(Sr[np.ix_(self.u_order_, self.u_order_)])
+                return
+            if matrix == 'Sc':
+                Sc = self.A_.T @ self.A_
+                plt.spy(Sc[np.ix_(self.v_order_, self.v_order_)])
+                return
+        else:
+            if matrix == 'A':
+                plt.spy(self.A_)
+                return
+            if matrix == 'Sr':
+                plt.spy(self.A_ @ self.A_.T)
+                return
+            if matrix == 'Sc':
+                plt.spy(self.A_.T @ self.A_)
+                return
+
+    def plot_u(self, transpose=False):
+        if transpose:
+            plt.plot(np.sort(self.u_), np.arange(self.u_.shape[0]))
+        else:
+            plt.plot(np.sort(self.u_))
+
+    def plot_v(self):
+        plt.plot(np.sort(self.v_))
---
+++ utils.py
@@ -0,0 +1,378 @@
+import os
+
+import numpy as np
+import pandas as pd
+import scipy.linalg as LA
+import scipy.sparse as sps
+from scipy.io import loadmat
+
+import matplotlib.pyplot as plt
+
+from scipy.spatial.distance import pdist, squareform
+from sklearn.neighbors import NearestNeighbors
+
+# PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets, transforms
+
+from tqdm import tqdm
+
+# animation
+from matplotlib import animation
+from celluloid import Camera
+
+# visualisation of the weights matrix
+import r1svd
+
+##########################################################################
+# Data Handling
+##########################################################################
+
+
+def fcps_reader(name, data_dir='../input/FCPS'):
+    points = pd.read_csv(f"{data_dir}/{name}.lrn",
+                         sep='\t', comment='%', index_col=0, header=None)
+    labels = pd.read_csv(f"{data_dir}/{name}.cls",
+                         sep='\t', comment='%', index_col=0, header=None)
+
+    points = points.rename(columns=lambda x: f"x{x}")
+    labels = labels.rename(columns={labels.columns[0]: 'y'})
+
+    return pd.concat([points, labels], axis=1)
+
+
+class FCPSDataset(Dataset):
+    """FCPS Dataset loader for PyTorch"""
+
+    def __init__(self, name, fcps_dir='../input/FCPS', transform=None):
+        data = fcps_reader(name, fcps_dir)
+        self.X = torch.tensor(data.iloc[:, :-1].values, dtype=torch.float)
+        self.y = torch.tensor(data.y.values, dtype=torch.int8)
+        self.transform = transform
+
+    def __len__(self):
+        return self.y.shape[0]
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        sample = self.X[idx, :], self.y[idx]
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+
+class ImageMatDataset(Dataset):
+    """Image Dataset from .mat files  
+    reads the files for: ORL, MNIST, 
+        COIL20, COIL100, Yale and USPS datasets
+    """
+
+    def __init__(self, name, img_dir='../input', transform=None):
+        mat = loadmat(f'{img_dir}/{name}.mat')
+        self.X = torch.tensor(mat['fea'], dtype=torch.float)
+        self.y = torch.tensor(mat['gnd'], dtype=torch.int8)
+        self.transform = transform
+
+    def __len__(self):
+        return self.y.shape[0]
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        sample = self.X[idx, :], self.y[idx]
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+
+##########################################################################
+# AutoEncoder
+##########################################################################
+
+class AutoEncoder(nn.Module):
+    def __init__(self, encode_sizes, decode_sizes=None):
+        super(AutoEncoder, self).__init__()
+        if decode_sizes is None:
+            decode_sizes = encode_sizes[::-1]
+
+        self.encoder = AutoEncoder.make_sequential(encode_sizes)
+        self.decoder = AutoEncoder.make_sequential(decode_sizes)
+        self.d_in = encode_sizes[0]
+        self.d_latent = encode_sizes[-1]
+
+    @staticmethod
+    def make_sequential(layer_sizes):
+        n = len(layer_sizes)
+        ls = layer_sizes
+        # create fully connected layers with ReLU activations
+        layers = [(nn.Linear(ls[i-1], ls[i]), nn.ReLU())
+                  for i in range(1, n-1)]
+        # unwrap tuples
+        layers = [fn for layer in layers for fn in layer]
+        # add the last layer
+        layers += [nn.Linear(ls[n-2], ls[n-1])]
+        # transform to sequential
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        enc = self.encoder(x)
+        dec = self.decoder(enc)
+        return enc, dec
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, x):
+        return self.decoder(x)
+
+
+##########################################################################
+# LLE implementations
+##########################################################################
+def get_neighbors(X, k):
+    n = X.shape[0]
+    knn = NearestNeighbors(algorithm='kd_tree').fit(X)
+    neighborhood = knn.kneighbors(X, n_neighbors=k+1, return_distance=False)
+
+    # filter points that are their own neighbors
+    x = np.arange(n)
+    mask = neighborhood != x[:, np.newaxis]
+    self_found = np.sum(~mask, axis=1) == 1
+    mask[:, -1] &= self_found
+
+    return neighborhood[mask].reshape((n, -1))
+
+    # dists = squareform(pdist(X))
+    # idx = np.arange(n)
+    # dists[idx == idx[:, np.newaxis]] = np.Inf
+    # # neighborhood = np.argsort(dists, axis=1)[:, :self.k]
+    # neighborhood = np.argpartition(dists, self.k+1, axis=1)[:, :self.k]
+
+
+class LLE:
+    def __init__(self, n_neighbors=5, d=2, reg=1E-3):
+        self.reg = reg
+        self.k = n_neighbors
+        self.d = d
+
+    def fit(self, X):
+        n, r = X.shape
+
+        # Step 1: compute pairwise distances
+        neighborhood = get_neighbors(X, self.k)
+
+        # Step 2: solve for the reconstruction weights W
+        W = np.zeros((n, n))
+        ones = np.ones(self.k)
+
+        for i in range(n):
+            nbrs = neighborhood[i, :]
+            z = X[nbrs, :] - X[i, :]
+            C = z @ z.T
+
+            # regularization
+            trace = float(C.trace())
+            C.flat[::self.k+1] += self.reg * trace if trace > 0 else self.reg
+
+    #         W[i, nbrs] = LA.pinv(C) @ ones
+            W[i, nbrs] = LA.solve(C, ones)
+
+    #     print(np.round(W, 2))
+        W = W / W.sum(axis=1)[:, np.newaxis]
+        self.S_ = W.copy()
+
+        # Step 3: compute the embedding from the eigenvectors
+        W.flat[::W.shape[0]+1] -= 1  # W - I
+        # calculate the embeddings
+        u, w, v = LA.svd(W)
+
+        idx = np.argsort(w)[1:(self.d+1)]
+        Y = v.T[:, idx]  # * n**.5
+
+        self.B_ = Y
+
+        return self
+
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.S_, self.B_
+
+
+class SparseLLE:
+    def __init__(self, n_neighbors=5, d=2, reg=1E-3):
+        self.reg = reg
+        self.k = n_neighbors
+        self.d = d
+
+    def fit(self, X):
+        n, r = X.shape
+
+        # Step 1: compute pairwise distances
+        neighborhood = get_neighbors(X, self.k)
+
+        # Step 2: solve for the reconstruction weights W
+        W = np.zeros((n, n))
+        ones = np.ones(self.k)
+
+        rows = np.empty(n*self.k, dtype=int)
+        cols = np.empty(n*self.k, dtype=int)
+        vals = np.empty(n*self.k)
+        for i in range(n):
+            nbrs = neighborhood[i, :]
+            z = X[nbrs, :] - X[i, :]
+            C = z @ z.T
+
+            # regularization
+            trace = float(C.trace())
+            C.flat[::self.k+1] += self.reg * trace if trace > 0 else self.reg
+
+            values = LA.solve(C, ones)
+
+            s, e = i*self.k, (i+1)*self.k
+            vals[s:e] = values
+            rows[s:e] = np.repeat(i, self.k)
+            cols[s:e] = nbrs
+
+        W = sps.csr_matrix((vals, (rows, cols)), shape=(n, n))
+    #     print(np.round(W.toarray(), 2))
+
+        w_sum = W.sum(axis=1).A.ravel()
+        w_sum_inv = sps.diags(1/w_sum)
+
+        W = w_sum_inv @ W
+
+        self.S_ = W.toarray().copy()
+
+        # Step 3: compute the embedding from the eigenvectors
+        I = sps.eye(n)
+        W = W - I
+        # calculate the embeddings
+        u, w, v = sps.linalg.svds(W, k=n-1, which='LM')
+    #     u, w, v = LA.svd(W.toarray())
+    #     w, v = sps.linalg.eigsh(W.T @ W, k=d+1, which='SM', sigma=0)
+    #     print(w.shape, v.shape)
+    #     v = v.T
+
+        idx = np.argsort(w)[:self.d]
+        Y = v.T[:, idx]  # * n**.5
+
+        self.B_ = Y
+
+        return self
+
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.S_, self.B_
+
+
+##########################################################################
+# Model Trainer
+##########################################################################
+
+class Trainer:
+
+    def __init__(self, net, loader, dataset, optimizer,
+                 n_epochs, n_neighbors=9, l=1, sparse_lle=False):
+        self.net = net
+        self.loader = loader
+        self.dataset = dataset
+        self.optimizer = optimizer
+        self.n_epochs = n_epochs
+        self.n_neighbors = n_neighbors
+        self.l = l
+        self.LLE = SparseLLE if sparse_lle else LLE
+
+    def train_model(self, track_W=False):
+        if track_W:
+            fig, ax = plt.subplots()
+            self.camera_ = Camera(fig)
+            r = r1svd.RankOneSvd()
+        else:
+            self.camera_ = None
+
+        losses = []
+        # loop over the dataset multiple times
+        for epoch in tqdm(range(self.n_epochs)):
+
+            for inputs, _ in self.loader:
+
+                # encode
+                enc, dec = self.net(inputs)
+                # compute
+                X_enc = enc.detach().numpy()
+                S, _ = self.LLE(self.n_neighbors,
+                                X_enc.shape[1]).fit_transform(X_enc)
+
+                cost = self.criterion(inputs, enc, dec, torch.tensor(
+                    S, dtype=torch.float),
+                    l=self.l if epoch <= self.n_epochs//3 else 1 + 1*(epoch - self.n_epochs))
+            #     print(f"Loss:\t{cost.item():.3f}")
+                losses.append(cost.item())
+
+                # update autoencoder weights
+                self.optimizer.zero_grad()
+                cost.backward()
+                self.optimizer.step()
+
+                if track_W:
+                    tmp = r.fit_transform(np.abs(S))
+                    ax.spy(tmp)
+                    self.camera_.snap()
+        if track_W:
+            plt.close(fig)
+
+        return losses
+
+    def predict(self, loader):
+        with torch.no_grad():
+            n = len(self.dataset)
+            d_ae = self.net.d_latent
+            d_in = self.net.d_in
+
+            X_ae = np.empty((n, d_ae))
+            batch_size = self.loader.batch_size
+            labels = np.empty(n)
+
+            X = np.empty((n, d_in))
+
+            for i, (inputs, outputs) in enumerate(loader):
+                x, _ = self.net(inputs)
+                x = x.numpy()
+                s = i*batch_size
+                e = min((i+1)*batch_size, n)
+                X[s:e, :] = inputs.numpy()
+                X_ae[s:e, :] = x
+                out = outputs.numpy().ravel()
+                if i == 0:
+                    labels = labels.astype(out.dtype)
+                labels[s:e] = out
+
+            S, Y = self.LLE(self.n_neighbors, d_ae).fit_transform(X_ae)
+
+            inp = torch.tensor(X, dtype=torch.float)
+            enc, dec = self.net(inp)
+            cost = Trainer.criterion(X, enc.numpy(), dec.numpy(), S, l=self.l)
+#             Y = LocallyLinearEmbedding(self.n_neighbors, d_ae).fit_transform(X_ae)
+        return S, X_ae, Y, labels, cost
+
+    @staticmethod
+    def criterion(X, encoded, decoded, S, l):
+        """Computes the autoencoder loss
+        loss = ||X - decoded||^2 + l * ||encoded - S*encoded||^2
+        the autoencoder should keep as much information while also
+        keeping the neighborhoods as separate as possible // could be better phrased
+        """
+        sim = X - decoded  # how similar the result is to the input
+        sep = encoded - S @ encoded  # how well separated the embedding is
+
+        return (sim ** 2).sum() + l * (sep ** 2).sum()
